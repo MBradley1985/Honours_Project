c269b210ef1e4b1615f9de53c201587b17d8c790
MBradley1985 <2496844@student.swin.edu.au>
2023-09-12 13:48:35 +1000

    ch ch ch changes

--- Metallicity_binned.py
+++ Metallicity_binned.py
@@ -65,14 +65,14 @@ def Metallicity_ihs(df, propertytoplot, titles, save_filename):
         w = np.where((halo >= 10.5) & (halo < 12.0) & (metals > 0))[0]
         metals_1 = metals[w]
         property_to_plot_1 = property_to_plot[w]
-        w1 = np.where((halo >= 12.01) & (halo < 13.5) & (metals > 0))[0]
+        w1 = np.where((halo >= 12.0) & (halo < 13.5) & (metals > 0))[0]
         metals_2 = metals[w1]
         property_to_plot_2 = property_to_plot[w1]
-        w2 = np.where((halo >= 13.51) & (halo < 17.5) & (metals > 0))[0]
+        w2 = np.where((halo >= 13.5) & (halo < 17.5) & (metals > 0))[0]
         metals_3 = metals[w2]
         property_to_plot_3 = property_to_plot[w2]
 
-        ax.scatter(property_to_plot_1, metals_1, marker='o', s=1, c='r', alpha=0.5, label=r'$10^{10.5} < M_{\mathrm{halo}} < 10^{12.0}$')
+        ax.scatter(property_to_plot_1, metals_1, marker='o', s=1, c='r', alpha=0.3, label=r'$10^{10.5} < M_{\mathrm{halo}} < 10^{12.0}$')
         ax.scatter(property_to_plot_2, metals_2, marker='o', s=1, c='g', alpha=0.5, label=r'$10^{12} < M_{\mathrm{halo}} < 10^{13.5}$')
         ax.scatter(property_to_plot_3, metals_3, marker='o', s=1, c='b', alpha=0.5, label=r'$10^{13.5} > M_{\mathrm{halo}}$')
 
@@ -120,14 +120,14 @@ def Metallicity_hmass(df, propertytoplot, titles, save_filename):
         w = np.where((halo >= 10.5) & (halo < 12.0) & (metals > 0))[0]
         metals_1 = metals[w]
         property_to_plot_1 = property_to_plot[w]
-        w1 = np.where((halo >= 12.01) & (halo < 13.5) & (metals > 0))[0]
+        w1 = np.where((halo >= 12.0) & (halo < 13.5) & (metals > 0))[0]
         metals_2 = metals[w1]
         property_to_plot_2 = property_to_plot[w1]
-        w2 = np.where((halo >= 13.51) & (halo < 17.5) & (metals > 0))[0]
+        w2 = np.where((halo >= 13.5) & (halo < 17.5) & (metals > 0))[0]
         metals_3 = metals[w2]
         property_to_plot_3 = property_to_plot[w2]
 
-        ax.scatter(property_to_plot_1, metals_1, marker='o', s=1, c='r', alpha=0.5, label=r'$10^{10.5} < M_{\mathrm{halo}} < 10^{12.0}$')
+        ax.scatter(property_to_plot_1, metals_1, marker='o', s=1, c='r', alpha=0.3, label=r'$10^{10.5} < M_{\mathrm{halo}} < 10^{12.0}$')
         ax.scatter(property_to_plot_2, metals_2, marker='o', s=1, c='g', alpha=0.5, label=r'$10^{12} < M_{\mathrm{halo}} < 10^{13.5}$')
         ax.scatter(property_to_plot_3, metals_3, marker='o', s=1, c='b', alpha=0.5, label=r'$10^{13.5} > M_{\mathrm{halo}}$')
 
@@ -175,14 +175,21 @@ def Metallicity_smass(df, propertytoplot, titles, save_filename):
         w = np.where((halo >= 10.5) & (halo < 12.0) & (metals > 0))[0]
         metals_1 = metals[w]
         property_to_plot_1 = property_to_plot[w]
-        w1 = np.where((halo >= 12.01) & (halo < 13.5) & (metals > 0))[0]
+        w1 = np.where((halo >= 12.0) & (halo < 13.5) & (metals > 0))[0]
         metals_2 = metals[w1]
         property_to_plot_2 = property_to_plot[w1]
-        w2 = np.where((halo >= 13.51) & (halo < 17.5) & (metals > 0))[0]
+        w2 = np.where((halo >= 13.5) & (halo < 17.5) & (metals > 0))[0]
         metals_3 = metals[w2]
         property_to_plot_3 = property_to_plot[w2]
 
-        ax.scatter(property_to_plot_1, metals_1, marker='o', s=1, c='r', alpha=0.5, label=r'$10^{10.5} < M_{\mathrm{halo}} < 10^{12.0}$')
+        # overplot Tremonti et al. 2003 (h=0.7)
+        w = np.arange(7.0, 13.0, 0.1)
+        Zobs = -1.492 + 1.847*w - 0.08026*w*w
+
+        # Conversion from Kroupa IMF to Slapeter IMF to Chabrier IMF
+        ax.plot(np.log10((10**w *1.5 /1.8)), Zobs, 'k', lw=2.0, label='Tremonti et al. 2003')
+
+        ax.scatter(property_to_plot_1, metals_1, marker='o', s=1, c='r', alpha=0.3, label=r'$10^{10.5} < M_{\mathrm{halo}} < 10^{12.0}$')
         ax.scatter(property_to_plot_2, metals_2, marker='o', s=1, c='g', alpha=0.5, label=r'$10^{12} < M_{\mathrm{halo}} < 10^{13.5}$')
         ax.scatter(property_to_plot_3, metals_3, marker='o', s=1, c='b', alpha=0.5, label=r'$10^{13.5} > M_{\mathrm{halo}}$')
 
@@ -230,14 +237,14 @@ def Metallicity_ihsfraction(df, propertytoplot, titles, save_filename):
         w = np.where((halo >= 10.5) & (halo < 12.0) & (metals > 0))[0]
         metals_1 = metals[w]
         property_to_plot_1 = property_to_plot[w]
-        w1 = np.where((halo >= 12.01) & (halo < 13.5) & (metals > 0))[0]
+        w1 = np.where((halo >= 12.0) & (halo < 13.5) & (metals > 0))[0]
         metals_2 = metals[w1]
         property_to_plot_2 = property_to_plot[w1]
-        w2 = np.where((halo >= 13.51) & (halo < 17.5) & (metals > 0))[0]
+        w2 = np.where((halo >= 13.5) & (halo < 17.5) & (metals > 0))[0]
         metals_3 = metals[w2]
         property_to_plot_3 = property_to_plot[w2]
 
-        ax.scatter(property_to_plot_1, metals_1, marker='o', s=1, c='r', alpha=0.5, label=r'$10^{10.5} < M_{\mathrm{halo}} < 10^{12.0}$')
+        ax.scatter(property_to_plot_1, metals_1, marker='o', s=1, c='r', alpha=0.3, label=r'$10^{10.5} < M_{\mathrm{halo}} < 10^{12.0}$')
         ax.scatter(property_to_plot_2, metals_2, marker='o', s=1, c='g', alpha=0.5, label=r'$10^{12} < M_{\mathrm{halo}} < 10^{13.5}$')
         ax.scatter(property_to_plot_3, metals_3, marker='o', s=1, c='b', alpha=0.5, label=r'$10^{13.5} > M_{\mathrm{halo}}$')
 
@@ -271,11 +278,16 @@ def Metallicity_ihsfraction(df, propertytoplot, titles, save_filename):
 
 # List of CSV files to process along with corresponding titles
 csv_files = ['/Users/michaelbradley/Documents/Honours/TAO/Small_sims_metallicity/tao.4455.0.csv',
+             '/Users/michaelbradley/Documents/Honours/TAO/Small_sims_metallicity/tao.4456.0.csv',
              '/Users/michaelbradley/Documents/Honours/TAO/Small_sims_metallicity/tao.4457.0.csv',
              '/Users/michaelbradley/Documents/Honours/TAO/Small_sims_metallicity/tao.4459.0.csv',
+             '/Users/michaelbradley/Documents/Honours/TAO/Small_sims_metallicity/tao.4460.0.csv',
              '/Users/michaelbradley/Documents/Honours/TAO/Small_sims_metallicity/tao.4461.0.csv',
-             '/Users/michaelbradley/Documents/Honours/TAO/Small_sims_metallicity/tao.4462.0.csv']  # Add your CSV filenames here
-titles = ['z = 0.00', 'z = 1.0', 'z = 2.07', 'z = 4.17', 'z = 5.288']  # Add corresponding titles
+             '/Users/michaelbradley/Documents/Honours/TAO/Small_sims_metallicity/tao.4462.0.csv',
+             '/Users/michaelbradley/Documents/Honours/TAO/Small_sims_metallicity/tao.4463.0.csv',
+             '/Users/michaelbradley/Documents/Honours/TAO/Small_sims_metallicity/tao.4464.0.csv',
+             '/Users/michaelbradley/Documents/Honours/TAO/Small_sims_metallicity/tao.4465.0.csv']  # Add your CSV filenames here
+titles = ['z = 0.00', 'z = 0.508', 'z = 1.0', 'z = 2.07', 'z = 3.06', 'z = 4.17', 'z = 5.288', 'z = 6.19', 'z = 8.54', 'z = 10.07']  # Add corresponding titles
 
 # Initialize lists to store DataFrames for old and new datasets
 datasets = []
